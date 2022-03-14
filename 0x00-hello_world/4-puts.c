9b41f40b06bf11565f5e225fc2defc2be8b20c97
CHrisDataHub <chrisdatahub@gmail.com>
2022-03-14 07:18:15 -0700

    print f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-/*commenting things
- * main start
- */
-
+/**
+*main - entry point
+*
+*Return: 0 after printing the function
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * My hello, printf task
+ * Return: return 1 after program execution
+ */
+
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a pieceof art,\n");
+	return (0);
+}	
